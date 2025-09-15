1520ce3f11846b70bf3b8ca0557de5ea9c63cf01
Saymonton Loback <saymonton@intronbrasil.com.br>
2025-09-15 11:13:24 -0300

    ADDED: .xaml.cs files

---
+++ Views/MonitoringView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Intron.LaserMonitor.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Intron.LaserMonitor.Views
+{
+    /// <summary>
+    /// Interação lógica para MonitoringView.xam
+    /// </summary>
+    public partial class MonitoringView : UserControl
+    {
+        private MonitoringViewModel ViewModel
+        {
+            get;
+        }
+        public MonitoringView()
+        {
+            InitializeComponent();
+            ViewModel = App.GetService<MonitoringViewModel>();
+            DataContext = ViewModel;
+        }
+    }
+}
---
+++ Views/ShellWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Intron.LaserMonitor.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Intron.LaserMonitor.Views
+{
+    /// <summary>
+    /// Lógica interna para ShellWindow.xaml
+    /// </summary>
+    public partial class ShellWindow : Window
+    {
+        public ShellViewModel ViewModel
+        {
+            get;
+        }
+        public ShellWindow()
+        {
+            InitializeComponent();
+            ViewModel = App.GetService<ShellViewModel>();
+            DataContext = ViewModel;
+        }
+    }
+}
